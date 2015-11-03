37393e1f637c3472246909c747a1d27714dfb522
Eric Diven <eric.diven@teradata.com>
2015-11-03 10:56:35 -0500

    Tidied stuff up

---
+++ build-all.sh
@@ -0,0 +1,12 @@
+IMAGES=("teradatalabs/centos6-ssh-oj8" \
+	"teradatalabs/hdp2.3-repo" \
+	"teradatalabs/hdp2.3-base" \
+	"teradatalabs/hdp2.3-master" \
+	"teradatalabs/hdp2.3-slave")
+
+for image in "${IMAGES[@]}"; do
+	echo $image
+	cd $image
+	docker build -t "$image" .
+	cd -
+done
