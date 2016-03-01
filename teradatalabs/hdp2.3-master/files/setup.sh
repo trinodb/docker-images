dd957d7f3a1de48938c980f35f2faadf657f39f2
Anton Petrov <anton.petrov@teradata.com>
2016-03-01 13:26:12 -0500

    Fix errors caused by switch to internal RPM mirror repo
    - The switch to an internal TD mirror of the HDP 2.3 artifacts
      caused a couple of errors in the image building and this commit
      takes care of these failures.
    - The Zookeeper initialization portion is being removed because
      it was never succeeding (even prior to the repo switch) and is
      generally not required.

--- teradatalabs/hdp2.3-master/files/setup.sh
+++ teradatalabs/hdp2.3-master/files/setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -ex
+
 # 1 format namenode
 chown hdfs:hdfs /var/lib/hadoop-hdfs/cache/
 su -c "echo 'N' | hdfs namenode -format" hdfs
@@ -11,7 +13,12 @@ su -c "hdfs namenode  2>&1 > /var/log/hadoop/hdfs/hadoop-hdfs-namenode.log" hdfs
 sleep 10
 
 # 4 exec hdp hdfs init script
-/usr/hdp/2.3.2.0-2950/hadoop/libexec/init-hdfs.sh
+# Failure on error is disabled and then enabled here because the
+# init-hdfs.sh script attempts to upload a jar to HDFS and that fails
+# because there is no Hadoop cluster up.
+set +e
+/usr/hdp/2.3.*/hadoop/libexec/init-hdfs.sh
+set -e
 
 # 4.1 Create an hdfs home directory for the yarn user. For some reason, init-hdfs doesn't do so.
 su -s /bin/bash hdfs -c '/usr/bin/hadoop fs -mkdir /user/yarn && /usr/bin/hadoop fs -chown yarn:yarn /user/yarn'
@@ -30,18 +37,15 @@ mysql_install_db
 /usr/bin/mysqld_safe &
 sleep 10s
 
+cp /usr/hdp/2.3.*/hive/scripts/metastore/upgrade/mysql/hive-schema-0.14.0.mysql.sql /tmp/hive-schema-0.14.0.mysql.sql
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql
-echo "CREATE DATABASE metastore; USE metastore; SOURCE /usr/hdp/2.3.2.0-2950/hive/scripts/metastore/upgrade/mysql/hive-schema-0.14.0.mysql.sql;" | mysql
+echo "CREATE DATABASE metastore; USE metastore; SOURCE /usr/hdp/2.3.0.0-2557/hive/scripts/metastore/upgrade/mysql/hive-schema-0.14.0.mysql.sql;" | mysql
 /usr/bin/mysqladmin -u root password 'root'
+rm -f /tmp/hive-schema-0.14.0.mysql.sql
 
 killall mysqld
 sleep 10s
 mkdir /var/log/mysql/
 chown mysql:mysql /var/log/mysql/
-
-# 8 Init zookeeper
-mkdir -p /var/lib/zookeeper
-chown zookeeper:hadoop /var/lib/zookeeper
-/usr/hdp/2.3.2.0-2950/zookeeper/etc/rc.d/init.d/zookeeper-server init
 
 exit 0
