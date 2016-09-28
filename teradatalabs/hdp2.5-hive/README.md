a1f51bb63329a18d2de79cf75856d645f645265a
Artur Gajowy <artur.gajowy@gmail.com>
2016-09-28 13:02:28 +0200

    Add hdp2.5-hive image

---
+++ teradatalabs/hdp2.5-hive/README.md
@@ -0,0 +1,6 @@
+# hdp2.5-hive [![][layers-badge]][layers-link] [![][version-badge]][dockerhub-link]
+           
+[layers-badge]: https://images.microbadger.com/badges/image/teradatalabs/hdp2.5-hive.svg
+[layers-link]: https://microbadger.com/images/teradatalabs/hdp2.5-hive
+[version-badge]: https://images.microbadger.com/badges/version/teradatalabs/hdp2.5-hive.svg
+[dockerhub-link]: https://hub.docker.com/r/teradatalabs/hdp2.5-hive
---
+++ teradatalabs/hdp2.5-hive/files/setup.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+set -ex
+
+# 1 format namenode
+chown hdfs:hdfs /var/lib/hadoop-hdfs/cache/
+su -c "echo 'N' | hdfs namenode -format" hdfs
+
+# 2 start hdfs
+su -c "hdfs namenode  2>&1 > /var/log/hadoop/hdfs/hadoop-hdfs-namenode.log" hdfs&
+
+# 3 wait for process starting
+sleep 10
+
+# 4 init basic hdfs directories
+/usr/hdp/2.5.0.0-1245/hadoop/libexec/init-hdfs.sh
+
+# 4.1 Create an hdfs home directory for the yarn user. For some reason, init-hdfs doesn't do so.
+su -s /bin/bash hdfs -c '/usr/bin/hadoop fs -mkdir /user/yarn && /usr/bin/hadoop fs -chown yarn:yarn /user/yarn'
+
+# 5 init hive directories
+su -s /bin/bash hdfs -c '/usr/bin/hadoop fs -mkdir /user/hive/warehouse'
+su -s /bin/bash hdfs -c '/usr/bin/hadoop fs -chmod 1777 /user/hive/warehouse'
+su -s /bin/bash hdfs -c '/usr/bin/hadoop fs -chown hive /user/hive/warehouse'
+
+# 6 stop hdfs
+killall java
+
+# 7 setup metastore
+mysql_install_db
+
+/usr/bin/mysqld_safe &
+sleep 10s
+
+cd /usr/hdp/2.5.*/hive/scripts/metastore/upgrade/mysql/
+echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql
+echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-1.2.1000.mysql.sql;" | mysql
+/usr/bin/mysqladmin -u root password 'root'
+
+killall mysqld
+sleep 10s
+mkdir /var/log/mysql/
+chown mysql:mysql /var/log/mysql/
+
+exit 0
