9db924f3687bc5845a273841ed5edb3366d52554
Shubham Tagra <stagra@qubole.com>
2019-06-27 09:19:01 +0530

    Use hdp3 on centos7 in hdp3.1-hive

--- prestodev/hdp3.1-hive/files/root/setup.sh
+++ prestodev/hdp3.1-hive/files/root/setup.sh
@@ -6,6 +6,12 @@ echo "127.0.0.1 hadoop-master" >> /etc/hosts
 # 1 format namenode
 chown hdfs:hdfs /var/lib/hadoop-hdfs/cache/
 
+mkdir -p /usr/hdp/3.1.0.0-78/hadoop/logs
+mkdir -p /var/log/hadoop-hdfs
+mkdir -p  /var/log/hadoop-yarn
+sudo chgrp -R hadoop /usr/hdp/3.1.0.0-78/hadoop/logs /var/log/hadoop-hdfs /var/log/hadoop-yarn
+chmod -R 770 /usr/hdp/3.1.0.0-78/hadoop/logs /var/log/hadoop-hdfs /var/log/hadoop-yarn
+
 # workaround for 'could not open session' bug as suggested here:
 # https://github.com/docker/docker/issues/7056#issuecomment-49371610
 rm -f /etc/security/limits.d/hdfs.conf
@@ -39,15 +45,21 @@ killall java
 # 7 setup metastore
 mysql_install_db
 
+# Without this getting perm error https://dba.stackexchange.com/questions/56441/cant-find-file-mysql-plugin-frm-when-starting-up-mysql
+chown -R mysql:mysql /var/lib/mysql
+chgrp -R mysql /var/lib/mysql
+rm -f  /var/lib/mysql/ib_logfile0
+rm -f  /var/lib/mysql/ib_logfile1
+
 /usr/bin/mysqld_safe &
 sleep 10s
 
 cd /usr/hdp/current/hive-metastore/scripts/metastore/upgrade/mysql/
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql
-echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-1.2.1000.mysql.sql;" | mysql
+echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-3.1.0.mysql.sql;" | mysql
 /usr/bin/mysqladmin -u root password 'root'
 
 killall mysqld
 sleep 10s
 mkdir /var/log/mysql/
-chown mysql:mysql /var/log/mysql/
+chown -R mysql:mysql /var/log/mysql/
