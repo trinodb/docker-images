3c96b4bc7979ae70e56285413fbea3fff9107982
Shubham Tagra <stagra@qubole.com>
2019-06-27 09:19:01 +0530

    Update hdp3.1-hive to HDP 3.1

--- prestodev/hdp3.1-hive/files/root/setup.sh
+++ prestodev/hdp3.1-hive/files/root/setup.sh
@@ -6,6 +6,10 @@ echo "127.0.0.1 hadoop-master" >> /etc/hosts
 # 1 format namenode
 chown hdfs:hdfs /var/lib/hadoop-hdfs/cache/
 
+mkdir /usr/hdp/current/hadoop-client/logs /var/log/hadoop-hdfs /var/log/hadoop-yarn
+chgrp -R hadoop /usr/hdp/current/hadoop-client/logs /var/log/hadoop-hdfs /var/log/hadoop-yarn
+chmod -R 770 /usr/hdp/current/hadoop-client/logs /var/log/hadoop-hdfs /var/log/hadoop-yarn
+
 # workaround for 'could not open session' bug as suggested here:
 # https://github.com/docker/docker/issues/7056#issuecomment-49371610
 rm -f /etc/security/limits.d/hdfs.conf
@@ -39,15 +43,17 @@ killall java
 # 7 setup metastore
 mysql_install_db
 
+chown -R mysql:mysql /var/lib/mysql
+
 /usr/bin/mysqld_safe &
 sleep 10s
 
 cd /usr/hdp/current/hive-metastore/scripts/metastore/upgrade/mysql/
 echo "GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION; FLUSH PRIVILEGES;" | mysql
-echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-1.2.1000.mysql.sql;" | mysql
+echo "CREATE DATABASE metastore; USE metastore; SOURCE hive-schema-3.1.0.mysql.sql;" | mysql
 /usr/bin/mysqladmin -u root password 'root'
 
 killall mysqld
 sleep 10s
 mkdir /var/log/mysql/
-chown mysql:mysql /var/log/mysql/
+chown -R mysql:mysql /var/log/mysql/
