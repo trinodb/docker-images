67511ebffdb3147cd0cc34877034e038ce87e192
Adam J. Shook <shook@datacatessen.com>
2020-08-24 22:37:42 +0000

    Initial commit for Greenplum Docker container

---
+++ prestodev/gpdb-6/files/home/gpadmin/.bash_profile
@@ -0,0 +1,2 @@
+export MASTER_DATA_DIRECTORY=/gpmaster/gpsne-1
+source /opt/greenplum-db/greenplum_path.sh
---
+++ prestodev/gpdb-6/files/usr/local/bin/psql.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Wrapper script to access GPDB via psql
+
+su gpadmin -c "source ${GPHOME}/greenplum_path.sh; psql ${DATABASE}"
