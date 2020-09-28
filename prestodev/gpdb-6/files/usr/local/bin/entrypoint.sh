b340d79157b730116294c57fe32af80e6ffe2a7d
Adam J. Shook <shook@datacatessen.com>
2020-09-28 14:46:38 +0000

    Add original hostname to etc/hosts file for Greenplum
    Greenplum keeps track of the hostname in the metadata after initialization,
    which can cause some issues as the hostnames change between Docker containers.
    Here we add the original hostname when GPDB was initialized and add it to
    /etc/hosts on startup so it can resolve that hostname.

--- prestodev/gpdb-6/files/usr/local/bin/entrypoint.sh
+++ prestodev/gpdb-6/files/usr/local/bin/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+echo "127.0.0.1 $(cat ~/original_hostname)" >> /etc/hosts
+
 service ssh start
 su gpadmin -l -c "export MASTER_DATA_DIRECTORY=/gpmaster/gpsne-1 ; source ${GPHOME}/greenplum_path.sh ; gpstart -a ; createdb ${DATABASE}"
 tail -f /gpmaster/gpsne-1/pg_log/gpdb-*.csv
