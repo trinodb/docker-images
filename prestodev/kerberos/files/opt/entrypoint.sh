6cd63f8b8b1ffb6aa051c158a468e3b3a7f96158
Adam J. Shook <shook@datacatessen.com>
2020-10-05 21:04:24 +0000

    Allow running multiple bootstrap commands in Krb
    Co-authored-by: Łukasz Walkiewicz <lukasz.walkiewicz@starburstdata.com>

--- prestodev/kerberos/files/opt/entrypoint.sh
+++ prestodev/kerberos/files/opt/entrypoint.sh
@@ -8,4 +8,10 @@ if [[ -v PRESTODEV_POST_BOOTSTRAP_COMMAND ]]; then
     $PRESTODEV_POST_BOOTSTRAP_COMMAND
 fi
 
+if test -d /docker/kerberos-init.d; then
+    for init_script in /docker/kerberos-init.d/*; do
+        "${init_script}"
+    done
+fi
+
 wait
