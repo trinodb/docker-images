ceaba87b31a83d3b3b6accf0c64aae3819e18469
Adam J. Shook <shook@datacatessen.com>
2020-10-06 13:12:23 +0000

    Allow running post-init scripts in Greenplum

--- prestodev/gpdb-6/files/usr/local/bin/entrypoint.sh
+++ prestodev/gpdb-6/files/usr/local/bin/entrypoint.sh
@@ -4,4 +4,11 @@ echo "127.0.0.1 $(cat ~/original_hostname)" >> /etc/hosts
 
 service ssh start
 su gpadmin -l -c "export MASTER_DATA_DIRECTORY=/gpmaster/gpsne-1 ; source ${GPHOME}/greenplum_path.sh ; gpstart -a ; createdb ${DATABASE}"
+
+if test -d /docker/gpdb-init.d; then
+    for init_script in /docker/gpdb-init.d/*; do
+        "${init_script}"
+    done
+fi
+
 tail -f /gpmaster/gpsne-1/pg_log/gpdb-*.csv
