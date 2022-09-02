4cf67fcf73d386735e26815a36a845fae6ef9d4f
Jan Waś <jan.was@starburstdata.com>
2022-09-02 15:42:59 +0200

    Format all shell scripts using shfmt

--- bin/depend.sh
+++ bin/depend.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 usage() {
-	echo "$0 {-d|-g|-p {tag}|-x} {target image Dockerfile} [known image tags]" >&2
+    echo "$0 {-d|-g|-p {tag}|-x} {target image Dockerfile} [known image tags]" >&2
 }
 
 find_parent() {
-	awk '
+    awk '
 		BEGIN {
 			ec = 1;
 		}
@@ -28,9 +28,9 @@ find_parent() {
 }
 
 contains() {
-	needle=$1
-	shift
-	echo "$@" | grep -q -E "\<$needle\>"
+    needle=$1
+    shift
+    echo "$@" | grep -q -E "\<$needle\>"
 }
 
 #
@@ -40,43 +40,43 @@ contains() {
 # invoking `docker pull' to fetch the base images.
 #
 make_friendly_name() {
-	echo "$1" | sed 's/:/@/g'
+    echo "$1" | sed 's/:/@/g'
 }
 
 untag() {
-	echo "${1%:*}"
+    echo "${1%:*}"
 }
 
 noop() {
-	:
+    :
 }
 
 depfiles_own_image() {
-	local target_image=$1
-	local make_friendly_parent=$(make_friendly_name "$2")
-	local untagged_parent=$(untag "$2")
+    local target_image=$1
+    local make_friendly_parent=$(make_friendly_name "$2")
+    local untagged_parent=$(untag "$2")
 
-	echo "$target_image@latest: $make_friendly_parent"
-	echo ".PHONY: $target_image.dependants $untagged_parent.dependants"
-	echo "$untagged_parent.dependants: $target_image"
-	echo "$untagged_parent.dependants: $target_image.dependants"
+    echo "$target_image@latest: $make_friendly_parent"
+    echo ".PHONY: $target_image.dependants $untagged_parent.dependants"
+    echo "$untagged_parent.dependants: $target_image"
+    echo "$untagged_parent.dependants: $target_image.dependants"
 }
 
 depfiles_ext_image() {
-	local target_image="$1"
-	local make_friendly_parent=$(make_friendly_name "$2")
+    local target_image="$1"
+    local make_friendly_parent=$(make_friendly_name "$2")
 
-	echo "$target_image@latest: $make_friendly_parent"
+    echo "$target_image@latest: $make_friendly_parent"
 }
 
 list_ext_image() {
-	local make_friendly_parent=$(make_friendly_name "$2")
-	echo "$make_friendly_parent"
+    local make_friendly_parent=$(make_friendly_name "$2")
+    echo "$make_friendly_parent"
 }
 
 graph_own_image() {
-	local untagged_parent=$(untag "$2")
-	cat <<-EOF
+    local untagged_parent=$(untag "$2")
+    cat <<-EOF
 	"$1" [shape=box]
 	"$untagged_parent" [shape=box]
 	"$1" -> "$untagged_parent"
@@ -85,7 +85,7 @@ EOF
 }
 
 graph_ext_image() {
-	cat <<-EOF
+    cat <<-EOF
 	"$1" [shape=box]
 	"$2" [shape=house; style=filled; fillcolor="#a0a0a0"]
 	"$1" -> "$2"
@@ -93,50 +93,50 @@ EOF
 }
 
 require_parent_tag() {
-	local target_image=$1
-	local parent_image=$2
+    local target_image=$1
+    local parent_image=$2
 
-	if ! echo "$parent_image" | grep ":${required_parent_tag}\$"; then
-		echo "FROM in Dockerfile for $target_image must specify a parent with the tag '$required_parent_tag'" >&2
-		exit 1
-	fi
+    if ! echo "$parent_image" | grep ":${required_parent_tag}\$"; then
+        echo "FROM in Dockerfile for $target_image must specify a parent with the tag '$required_parent_tag'" >&2
+        exit 1
+    fi
 }
 
 while getopts ":dgp:x" c; do
-	case $c in
-		d)
-			own_image_function=depfiles_own_image
-			ext_image_function=depfiles_ext_image
-			;;
-		x)
-			own_image_function=noop
-			ext_image_function=list_ext_image
-			;;
-		g)
-			own_image_function=graph_own_image
-			ext_image_function=graph_ext_image
-			;;
-		p)
-			own_image_function=require_parent_tag
-			required_parent_tag=$OPTARG
-			ext_image_function=noop
-			;;
-		\?)
-			echo "Unrecognized option -$OPTARG" >&2
-			exit 1
-			;;
-		:)
-			echo "Option -$OPTARG requires an argument" >&2
-			exit 1
-			;;
-	esac
+    case $c in
+        d)
+            own_image_function=depfiles_own_image
+            ext_image_function=depfiles_ext_image
+            ;;
+        x)
+            own_image_function=noop
+            ext_image_function=list_ext_image
+            ;;
+        g)
+            own_image_function=graph_own_image
+            ext_image_function=graph_ext_image
+            ;;
+        p)
+            own_image_function=require_parent_tag
+            required_parent_tag=$OPTARG
+            ext_image_function=noop
+            ;;
+        \?)
+            echo "Unrecognized option -$OPTARG" >&2
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument" >&2
+            exit 1
+            ;;
+    esac
 done
 
-shift $((OPTIND-1))
+shift $((OPTIND - 1))
 
 if [ -z "$own_image_function" ] || [ $# -lt 2 ]; then
-	usage
-	exit 1
+    usage
+    exit 1
 fi
 
 target_dockerfile=$1
@@ -147,19 +147,19 @@ known_images="$*"
 parent_image_tag=$(find_parent "$target_dockerfile")
 ec=$?
 case $ec in
-	0) ;;
-	1)
-		echo "Failed to find a parent docker image in $target_dockerfile" >&2
-		exit $ec
-		;;
-	2)
-		echo "Found multiple parent docker images in $target_dockerfile" >&2
-		exit $ec
-		;;
+    0) ;;
+    1)
+        echo "Failed to find a parent docker image in $target_dockerfile" >&2
+        exit $ec
+        ;;
+    2)
+        echo "Found multiple parent docker images in $target_dockerfile" >&2
+        exit $ec
+        ;;
 esac
 
 if contains "$parent_image_tag" "$known_images"; then
-	$own_image_function "$target_image" "$parent_image_tag"
+    $own_image_function "$target_image" "$parent_image_tag"
 else
-	$ext_image_function "$target_image" "$parent_image_tag"
+    $ext_image_function "$target_image" "$parent_image_tag"
 fi
--- bin/test.sh
+++ bin/test.sh
@@ -3,46 +3,46 @@
 set -e
 
 function retry() {
-  END=$(($(date +%s) + 600))
+    END=$(($(date +%s) + 600))
 
-  while (( $(date +%s) < $END )); do
-    set +e
-    "$@"
-    EXIT_CODE=$?
-    set -e
+    while (($(date +%s) < END)); do
+        set +e
+        "$@"
+        EXIT_CODE=$?
+        set -e
 
-    if [[ ${EXIT_CODE} == 0 ]]; then
-      break
-    fi
-    sleep 5
-  done
+        if [[ ${EXIT_CODE} == 0 ]]; then
+            break
+        fi
+        sleep 5
+    done
 
-  return ${EXIT_CODE}
+    return ${EXIT_CODE}
 }
 
 function environment_compose() {
-  docker-compose -f "${DOCKER_CONF_LOCATION}/${ENVIRONMENT}/docker-compose.yml" "$@"
+    docker-compose -f "${DOCKER_CONF_LOCATION}/${ENVIRONMENT}/docker-compose.yml" "$@"
 }
 
 function check_hadoop() {
-  environment_compose exec hadoop-master hive -e 'select 1;' > /dev/null 2>&1
+    environment_compose exec hadoop-master hive -e 'select 1;' >/dev/null 2>&1
 }
 
 function run_hadoop_tests() {
-  environment_compose exec hadoop-master hive -e 'SELECT 1' &&
-  environment_compose exec hadoop-master hive -e 'CREATE TABLE foo (a INT);' &&
-  environment_compose exec hadoop-master hive -e 'INSERT INTO foo VALUES (54);' &&
-  # SELECT with WHERE to make sure that map-reduce job is scheduled
-  environment_compose exec hadoop-master hive -e 'SELECT a FROM foo WHERE a > 0;' &&
-  # Test table bucketing
-  environment_compose exec hadoop-master hive -e '
+    environment_compose exec hadoop-master hive -e 'SELECT 1' &&
+        environment_compose exec hadoop-master hive -e 'CREATE TABLE foo (a INT);' &&
+        environment_compose exec hadoop-master hive -e 'INSERT INTO foo VALUES (54);' &&
+        # SELECT with WHERE to make sure that map-reduce job is scheduled
+        environment_compose exec hadoop-master hive -e 'SELECT a FROM foo WHERE a > 0;' &&
+        # Test table bucketing
+        environment_compose exec hadoop-master hive -e '
     CREATE TABLE bucketed_table(a INT) CLUSTERED BY(a) INTO 32 BUCKETS;
     SET hive.enforce.bucketing = true;
     INSERT INTO bucketed_table VALUES (1), (2), (3), (4);
   ' &&
-  test $(environment_compose exec hadoop-master hdfs dfs -ls /user/hive/warehouse/bucketed_table \
-    | tee /dev/stderr | grep /bucketed_table/ | wc -l) -ge 4 &&
-  true
+        test $(environment_compose exec hadoop-master hdfs dfs -ls /user/hive/warehouse/bucketed_table |
+            tee /dev/stderr | grep /bucketed_table/ | wc -l) -ge 4 &&
+        true
 }
 
 function run_hive_transactional_tests() {
@@ -50,22 +50,22 @@ function run_hive_transactional_tests() {
       CREATE TABLE transactional_table (x int) STORED AS orc TBLPROPERTIES ('transactional'='true');
       INSERT INTO transactional_table VALUES (1), (2), (3), (4);
     " &&
-    environment_compose exec hadoop-master hive -e 'SELECT x FROM transactional_table WHERE x > 0;' &&
-    environment_compose exec hadoop-master hive -e 'DELETE FROM transactional_table WHERE x = 2;' &&
-    environment_compose exec hadoop-master hive -e 'UPDATE transactional_table SET x = 14 WHERE x = 4;' &&
-    environment_compose exec hadoop-master hive -e 'SELECT x FROM transactional_table WHERE x > 0;' &&
-    true
+        environment_compose exec hadoop-master hive -e 'SELECT x FROM transactional_table WHERE x > 0;' &&
+        environment_compose exec hadoop-master hive -e 'DELETE FROM transactional_table WHERE x = 2;' &&
+        environment_compose exec hadoop-master hive -e 'UPDATE transactional_table SET x = 14 WHERE x = 4;' &&
+        environment_compose exec hadoop-master hive -e 'SELECT x FROM transactional_table WHERE x > 0;' &&
+        true
 }
 
 function check_gpdb() {
-  environment_compose exec gpdb su gpadmin -l -c "pg_isready"
+    environment_compose exec gpdb su gpadmin -l -c "pg_isready"
 }
 
 function run_gpdb_tests() {
     environment_compose exec gpdb su gpadmin -l -c "psql -c 'CREATE TABLE foo (a INT) DISTRIBUTED RANDOMLY'" &&
-    environment_compose exec gpdb su gpadmin -l -c "psql -c 'INSERT INTO foo VALUES (54)'" &&
-    environment_compose exec gpdb su gpadmin -l -c "psql -c 'SELECT a FROM foo'" &&
-    true
+        environment_compose exec gpdb su gpadmin -l -c "psql -c 'INSERT INTO foo VALUES (54)'" &&
+        environment_compose exec gpdb su gpadmin -l -c "psql -c 'SELECT a FROM foo'" &&
+        true
 }
 
 function run_kerberos_tests() {
@@ -75,51 +75,50 @@ function run_kerberos_tests() {
 }
 
 function stop_all_containers() {
-  local ENVIRONMENT
-  for ENVIRONMENT in $(getAvailableEnvironments)
-  do
-     stop_docker_compose_containers ${ENVIRONMENT}
-  done
+    local ENVIRONMENT
+    for ENVIRONMENT in $(getAvailableEnvironments); do
+        stop_docker_compose_containers ${ENVIRONMENT}
+    done
 }
 
 function stop_docker_compose_containers() {
-  local ENVIRONMENT=$1
-  RUNNING_CONTAINERS=$(environment_compose ps -q)
+    local ENVIRONMENT=$1
+    RUNNING_CONTAINERS=$(environment_compose ps -q)
 
-  if [[ ! -z ${RUNNING_CONTAINERS} ]]; then
-    # stop containers started with "up", removing their volumes
-    # Some containers (SQL Server) fail to stop on Travis after running the tests. We don't have an easy way to
-    # reproduce this locally. Since all the tests complete successfully, we ignore this failure.
-    environment_compose down -v || true
-  fi
+    if [[ -n ${RUNNING_CONTAINERS} ]]; then
+        # stop containers started with "up", removing their volumes
+        # Some containers (SQL Server) fail to stop on Travis after running the tests. We don't have an easy way to
+        # reproduce this locally. Since all the tests complete successfully, we ignore this failure.
+        environment_compose down -v || true
+    fi
 
-  echo "Docker compose containers stopped: [$ENVIRONMENT]"
+    echo "Docker compose containers stopped: [$ENVIRONMENT]"
 }
 
 function cleanup() {
-  stop_docker_compose_containers ${ENVIRONMENT}
+    stop_docker_compose_containers ${ENVIRONMENT}
 
-  # Ensure that the logs processes are terminated.
-  # In most cases after the docker containers are stopped, logs processes must be terminated.
-  if [[ ! -z ${LOGS_PID} ]]; then
-    kill ${LOGS_PID} 2>/dev/null || true
-  fi
+    # Ensure that the logs processes are terminated.
+    # In most cases after the docker containers are stopped, logs processes must be terminated.
+    if [[ -n ${LOGS_PID} ]]; then
+        kill ${LOGS_PID} 2>/dev/null || true
+    fi
 
-  # docker logs processes are being terminated as soon as docker container are stopped
-  # wait for docker logs termination
-  wait 2>/dev/null || true
+    # docker logs processes are being terminated as soon as docker container are stopped
+    # wait for docker logs termination
+    wait 2>/dev/null || true
 }
 
 function terminate() {
-  trap - INT TERM EXIT
-  set +e
-  cleanup
-  exit 130
+    trap - INT TERM EXIT
+    set +e
+    cleanup
+    exit 130
 }
 
 function getAvailableEnvironments() {
-  for i in $(ls -d $DOCKER_CONF_LOCATION/*/); do echo ${i%%/}; done \
-     | grep -v files | grep -v common | xargs -n1 basename
+    for i in $(ls -d $DOCKER_CONF_LOCATION/*/); do echo ${i%%/}; done |
+        grep -v files | grep -v common | xargs -n1 basename
 }
 
 SCRIPT_DIR=${BASH_SOURCE%/*}
@@ -130,8 +129,8 @@ ENVIRONMENT=$1
 
 # Get the list of valid environments
 if [[ ! -f "$DOCKER_CONF_LOCATION/$ENVIRONMENT/docker-compose.yml" ]]; then
-   echo "Usage: run_on_docker.sh <$(getAvailableEnvironments | tr '\n' '|')>"
-   exit 1
+    echo "Usage: run_on_docker.sh <$(getAvailableEnvironments | tr '\n' '|')>"
+    exit 1
 fi
 
 shift 1
@@ -173,7 +172,7 @@ else
     sleep 10
     run_hadoop_tests
     if [[ ${ENVIRONMENT} == *"3.1-hive" ]]; then
-      run_hive_transactional_tests
+        run_hive_transactional_tests
     fi
 fi
 
